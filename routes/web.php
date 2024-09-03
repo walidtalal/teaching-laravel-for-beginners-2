f08692252328966dd9f935c94fcbdcc1f566e6fd
walidtalal <alwaleedhawas@gmail.com>
2024-09-03 16:35:20 +0300

    integrate admin theme

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::name('front.')->group(function () {
     Route::view('/contact', 'front.contact')->name('contact');
 });
 
+Route::name('admin.')->prefix('admin')->group(function () {
+    Route::view('/', 'admin.index')->name('index');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
