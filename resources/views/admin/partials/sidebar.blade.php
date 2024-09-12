23269278236e044eed29073cbb11726dde1b31c5
walidtalal <alwaleedhawas@gmail.com>
2024-09-12 16:00:33 +0300

    messages module

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -46,6 +46,10 @@
             {{-- features --}}
             <x-sidebar-tab href="{{ route('admin.features.index') }}" icon="fe-bookmark"
                            name="{{ __('keywords.features') }}"></x-sidebar-tab>
+
+            {{-- messages --}}
+            <x-sidebar-tab href="{{ route('admin.messages.index') }}" icon="fe-message-square"
+                           name="{{ __('keywords.messages') }}"></x-sidebar-tab>
         </ul>
 
     </nav>
