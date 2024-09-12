492d5b889db3c728f946d3981670abfafaf590de
walidtalal <alwaleedhawas@gmail.com>
2024-09-12 14:31:17 +0300

    features module

---
+++ resources/views/admin/features/index.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.master')
+
+@section('title', __('keywords.features'))
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between mb-3">
+                    <h2 class="h5 page-title">{{ __('keywords.features') }}</h2>
+
+                    <div class="page-title-right">
+                        <x-action-button href="{{ route('admin.features.create') }}" type="create"></x-action-button>
+                    </div>
+                </div>
+
+                <div class="card shadow">
+                    <div class="card-body">
+
+                        <x-success-alert></x-success-alert>
+
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th width="5%">#</th>
+                                <th>{{ __('keywords.title') }}</th>
+                                <th width="10%">{{ __('keywords.icon') }}</th>
+                                <th width="15%">{{ __('keywords.actions') }}</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @if (count($features) > 0)
+                                @foreach ($features as $key => $feature)
+                                    <tr>
+                                        <td>{{ $features->firstItem() + $loop->index }}</td>
+                                        <td>{{ $feature->title }}</td>
+                                        <td>{{ $feature->icon }}</td>
+                                        <td>
+                                            <x-action-button
+                                                href="{{ route('admin.features.edit', ['feature' => $feature]) }}"
+                                                type="edit"></x-action-button>
+
+                                            <x-action-button
+                                                href="{{ route('admin.features.show', ['feature' => $feature]) }}"
+                                                type="show"></x-action-button>
+
+                                            <x-delete-button
+                                                href="{{ route('admin.features.destroy', ['feature' => $feature]) }}"
+                                                id="{{ $feature->id }}"></x-delete-button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <x-empty-alert></x-empty-alert>
+                            @endif
+                            </tbody>
+                        </table>
+                        {{ $features->render('pagination::bootstrap-4') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -39,8 +39,13 @@
 {{--                </a>--}}
 {{--            </li>--}}
 
+            {{-- Services --}}
             <x-sidebar-tab href="{{ route('admin.services.index') }}" icon="fe-codesandbox"
                            name="{{ __('keywords.services') }}"></x-sidebar-tab>
+
+            {{-- features --}}
+            <x-sidebar-tab href="{{ route('admin.features.index') }}" icon="fe-bookmark"
+                           name="{{ __('keywords.features') }}"></x-sidebar-tab>
         </ul>
 
     </nav>
